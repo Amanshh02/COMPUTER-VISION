ec18a66cab4176a0864581e12ee05b26dcebc613
Amanshh02 <--hydrocarbon.aman02@gmail.com>
2025-06-18 00:48:10 +0530

    commit with lin algebra

--- numpy/lec-3_linear algebra.py
+++ numpy/lec-3_linear algebra.py
@@ -1,4 +1,17 @@
 import numpy as np
 
 # Linear Algebra
-a = np.full((2,3), 2)
+a = np.ones((2,3))
+print(a)
+
+b= np.full((3,2),2)
+print(b)
+
+# a*b is an error
+
+#multiplication
+print(np.matmul(a,b))
+
+#determinant
+c = np.identity(3)
+print(np.linalg.det(c))
